f15ac8f7a9c38da735a1d91500b8fdfccee41945
Hubert-Zhu <zhubolin010118@gmail.com>
2023-07-26 16:25:07 -0400

    Clean up AI checker

--- src/utils/sensitiveTextChecker.ts
+++ src/utils/sensitiveTextChecker.ts
@@ -5,17 +5,9 @@ export const isContentToxic = async (content: String) => {
   if(enable_AI_text_checker == false){
     return false
   }
-
-  const data = {
-    text: `${content}`
-  }
-
-  console.log(data)
-  console.log(JSON.stringify(data))
+  
   try {
     const response: AxiosResponse<any, any> = await axios.post('http://localhost:5000/predict', {text: `${content}`});
-
-    console.log(response.data.result)
     const result = await response.data.result
 
     if (result == "Toxic") {
