90df3a5d8958c82b80c944541b8ba6f1da34cdaf
Hubert-Zhu <zhubolin010118@gmail.com>
2023-07-26 23:03:12 -0400

    Add more info for User Profile

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -22,4 +22,16 @@ export class User {
 
     @Column({ nullable: true })
     verificationCode: string
+
+    @Column({ nullable: true })
+    phoneNumber: string; 
+
+    @Column({ nullable: true })
+    facebookProfile: string;
+
+    @Column({ nullable: true })
+    twitterProfile: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
 }
