980bdf49bea7e18841253a65a75d266054932388
Lizhuo You <mrlizhuoyou@gmail.com>
2024-03-08 10:44:05 -0500

    Use provided url

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,15 +1,40 @@
-import { DataSource } from 'typeorm'
+import { DataSource, DataSourceOptions } from 'typeorm'
+import * as PostgressConnectionStringParser from 'pg-connection-string'
 
-const myDataSource: DataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: Number(process.env.POSTGRES_PORT) || 5432,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  entities: ['src/entity/*.ts'],
-  logging: true,
-  synchronize: true,
-})
+let config: DataSourceOptions
+
+if (process.env.DATABASE_URL) {
+  const connectionOptions = PostgressConnectionStringParser.parse(
+    process.env.DATABASE_URL
+  )
+  config = {
+    type: 'postgres',
+    host: connectionOptions.host,
+    port: Number(connectionOptions.port),
+    username: connectionOptions.user,
+    password: connectionOptions.password,
+    database: connectionOptions.database,
+    entities: ['src/entity/*.ts'],
+    extra: {
+      ssl: true,
+    },
+    logging: true,
+    synchronize: true,
+  }
+} else {
+  config = {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: Number(process.env.POSTGRES_PORT) || 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    entities: ['src/entity/*.ts'],
+    logging: true,
+    synchronize: true,
+  }
+}
+
+const myDataSource: DataSource = new DataSource(config)
 
 export default myDataSource
