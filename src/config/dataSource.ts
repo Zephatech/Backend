a2bef663b4556e7fd2ed6552f77134f1a1881edf
Lizhuo You <mrlizhuoyou@gmail.com>
2024-03-08 02:30:41 -0500

    bug fix

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -2,7 +2,7 @@ import { DataSource } from 'typeorm'
 
 const myDataSource: DataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DATABASE_URL || 'localhost',
+  host: process.env.DATABASE_HOST || 'localhost',
   port: Number(process.env.POSTGRES_PORT) || 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
